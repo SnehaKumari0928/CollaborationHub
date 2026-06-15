b18adc994cda5df349a081298ebdf6d3342a44aa
Sneha Kumari <sneha.kumari@testingxperts.com>
2026-06-15 17:06:18 +0530

    register and login

--- backend/Services/Implementations/AuthService.cs
+++ backend/Services/Implementations/AuthService.cs
@@ -1,4 +1,6 @@
-﻿using backend.Repositories.Interfaces;
+﻿using backend.DTOs.Auth.Request;
+using backend.DTOs.Auth.Response;
+using backend.Repositories.Interfaces;
 using backend.Services.Interfaces;
 
 namespace backend.Services.Implementations
@@ -7,6 +9,17 @@ namespace backend.Services.Implementations
     {
         private readonly IUserRepository _userRepository;
 
+        public async Task RegisterAsync(RegisterRequestDto dto)
+        {
 
+        }
+        public async Task<AuthResponseDto> LoginAsync(LoginRequestDto dto)
+        {
+
+        }
+        public async Task LogoutAsync(string refreshToken)
+        {
+
+        }
     }
 }
